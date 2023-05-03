ec1018066b2a5eea496c7d7dee4e178e5d371a1d
Petar26920 <118538092+Petar26920@users.noreply.github.com>
2023-05-03 23:19:08 +0200

    Create 2023_05_03_205528_create_cv_table.php
    Dodali migracije za users i cv i povezail

---
+++ database/migrations/2023_05_03_205528_create_cv_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cv', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('users_id')->unsigned()->index();
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('Cv_name',30);
+            $table->timestamps();
+
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cv');
+    }
+};
