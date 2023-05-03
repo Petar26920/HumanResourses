231db46b9c73d0f75e2c8d2492e3bb7d56c45c3c
Petar26920 <118538092+Petar26920@users.noreply.github.com>
2023-05-03 21:51:07 +0200

    ProbaPravljenjaDB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('FirstName',30);
+            $table->string('LastName',30);
+            $table->string('Email',50)->unique();
+            $table->string('Role',1);
+            $table->string('PhoneNum',15);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('PassHash');
             $table->rememberToken();
             $table->timestamps();
         });
